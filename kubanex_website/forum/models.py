7e5f6468c8713e50b4104be21551ea9fcbd7e14b
Nikita Syrman <91823336+dr-livesey@users.noreply.github.com>
2023-02-13 17:23:35 +0300

    formatted by pep8

--- kubanex_website/forum/models.py
+++ kubanex_website/forum/models.py
@@ -9,6 +9,7 @@ from django.contrib.auth.models import User
 
 MAX_COMMENT_SLICE_SIZE = 48
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=1000, blank=True)
@@ -19,11 +20,13 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+
 class Thread(models.Model):
     name = models.CharField(max_length=100)
-    
+
     def __str__(self) -> str:
         return self.name
+
 
 class Post(models.Model):
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
@@ -32,9 +35,10 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self) -> str:
         return self.title
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
