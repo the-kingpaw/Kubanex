d752b4f2df2e8ae2c55f8250fd26faafee8b245c
Nikita Syrman <91823336+dr-livesey@users.noreply.github.com>
2023-02-13 10:16:25 +0300

    added __str__ to models

--- kubanex_website/forum/models.py
+++ kubanex_website/forum/models.py
@@ -7,6 +7,8 @@ from django.contrib.auth.models import User
 # - Post (may contain a few images and text (in markdown))
 # - Comment (only text)
 
+MAX_COMMENT_SLICE_SIZE = 48
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=1000, blank=True)
@@ -14,15 +16,25 @@ class Profile(models.Model):
     birth_date = models.DateField(null=True, blank=True)
     image = models.ImageField(upload_to='profile_image', blank=True)
 
+    def __str__(self):
+        return self.user.username
+
 class Thread(models.Model):
     name = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return self.name
 
 class Post(models.Model):
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.title
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -30,3 +42,6 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.content[:MAX_COMMENT_SLICE_SIZE]
