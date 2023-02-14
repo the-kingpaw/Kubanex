d27a01691bf477b4230b9f667d38e49233d26b99
Nikita Syrman <91823336+dr-livesey@users.noreply.github.com>
2023-02-14 06:46:42 +0300

    added basic navigation home<->sign_up

--- kubanex_website/forum/urls.py
+++ kubanex_website/forum/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import index, sign_up
 
+app_name = 'forum'
 urlpatterns = [
     path('', index, name='homepage'),
     path('register/', sign_up, name='registration'),
